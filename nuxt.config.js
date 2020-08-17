3f916a13c26801ff6843d21711e58385fc97ca5a
Jon Palustre <jonalecp@gmail.com>
2020-08-18 03:12:18 +0800

    Updated nuxt.config.js file again

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import { butter } from '~/plugins/buttercms'
-
 export default {
   mode: 'universal',
   /*
@@ -47,22 +45,5 @@ export default {
      */
     extend(config, ctx) {}
   },
-  target: 'static',
-  generate: {
-    routes() {
-      return butter.post
-
-        .list({
-          page: 1,
-
-          page_size: 10
-        })
-
-        .then((res) => {
-          return res.data.data.map((blog) => {
-            return `/blog/${blog.slug}`
-          })
-        })
-    }
-  }
+  target: 'static'
 }
